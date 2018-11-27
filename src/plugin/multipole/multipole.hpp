adde5ab6b9c9425eaf79e873021cec4308932303
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2018-11-27 11:28:40 -0200

    finishing dipole solver and defining mutipole layer.

--- src/plugin/multipole/multipole.hpp
+++ src/plugin/multipole/multipole.hpp
@@ -1,12 +1,48 @@
+#pragma once
+
+#ifndef MULTIPOLE_HPP_INCLUDED
+#define MULTIPOLE_HPP_INCLUDED
+
+// --------------------------- Dipole Solver ---------------------------------------------------
+
 class DipoleSolver
 {
 public:
-  DipoleSolver(Float sigma_a, Float sigma_s_prime, Floaat d);
+  DipoleSolver(Float eta0, Float eta1, Float sigma_a, Float sigma_s_prime, Float thickness);
 
   Float R(Float r);
   Float T(Float r);
+
+private:
+	Float A(Float rho_d);
+	Float FresnelDiffuseReflectance(float eta);
+
 private:
   Float sigma_a;
   Float sigma_s_prime;
+  Float sigma_t_prime;
+  Float alpha_prime;
+  Float sigma_tr;
+  Float Fdr;
+  Float zr;
+  Float zv;
+  Float dr;
+  Float dv;
   Float d;
 };
+
+// ------------------------ Multipole Layer ------------------------------------------------------
+struct MultipoleLayer
+{
+	Float eta0;
+	Float eta1;
+	Float thickness;
+	Float sigma_a;
+	Float sigma_s_prime;
+};
+
+
+// -------------------- Multipole Table ---------------------------------------------------------
+
+
+#endif
--- src/plugin/multipole/mutipole.cpp
+++ src/plugin/multipole/mutipole.cpp
@@ -2,22 +2,45 @@
 #include "../../core/pbrt.h"
 #include <cmath>
 
-DipoleSolver::DipoleSolver(Float sigma_a, Float sigma_s_prime, Float d):
+
+// --------------------------- Dipole Solver -----------------------------------------------------------------------
+DipoleSolver::DipoleSolver(Float eta0, Float eta1, Float sigma_a, Float sigma_s_prime, Float thickness):
   sigma_a{a},
   sigma_s_prime{sigma_s_prime},
-  d{d}
-{}
+  d{thickness}
+{
+  sigma_t_prime = sigma_a + sigma_s_prime;
+  alpha_prime = sigma_s_prime / sigma_t_prime;
+  sigma_tr = sqrtf(3*sigma_a*sigma_t_prime);
+  
+  // TODO: check with eta = eta0/eta1 (from the term ratio of index of refraction as described on the paper.  
+  Fdr = FresnelDiffuseReflectance(eta0/eta1);
+
+  zr = (1.f / sigma_t_prime);
+  zv = (1.f + 4.f*A(Fdr)/3.f)/sigma_t_prime;
+
+  dr = sqrt(r*r + zr*zr);
+  dv = sqrt(r*r + zv*zv);
+}
+
+Float DipoleSolver::A(Float Fdr)
+{
+  return (1.f + Fdr) / (1.f - Fdr);
+}
+
+Float FresnelDiffuseReflectance(float eta)
+{
+  if (eta >= 1.f) {
+    return -(1.4399f/(eta*eta)) + (0.7099f/eta) + 0.6681f + 0.0636f*eta; 
+  }
+  else {
+    Float eta2 = eta*eta;
+    return -0.4399f + (0.7099/eta) - (0.3319/eta2) + (0.0636/(eta2*eta));
+  }
+}
 
 Float DipoleSolver::R(Float r)
 {
-  Float sigma_t_prime = sigma_a + sigma_s_prime;
-  Float alpha_prime = sigma_s_prime / sigma_t_prime;
-  Float sigma_tr = sqrtf(3*sigma_a*sigma_t_prime);
-  Float zr = (1.f / sigma_t_prime);
-  Float zv = (1.f + 4.f*A/3.f)/sigma_t_prime;
-
-  Float dr = sqrt(r*r + zr*zr);
-  Float dv = sqrt(r*r + zv*zv);
   Float D = 1.f/(3.f*sigma_t_prime);
 
   Float denTerm1 = alpha_prime * zr * (1 + sigma_tr * dr) * exp(-sigma_tr*dr);
@@ -30,13 +53,6 @@ Float DipoleSolver::R(Float r)
 
 Float DipoleSolver::T(Float r)
 {
-  Float alpha_prime = sigma_s_prime / sigma_t_prime;
-  Float sigma_tr = sqrtf(3*sigma_a*sigma_t_prime);
-  Float zr = (1.f / sigma_t_prime);
-  Float zv = (1.f + 4.f*A/3.f)/sigma_t_prime;
-  Float dr = sqrt(r*r + zr*zr);
-  Float dv = sqrt(r*r + zv*zv);
-
   Float denTerm1 = alpha_prime*(d - zr) * (1.f + sigma_tr*dr) * exp(-sigma_tr*dr);
   Float numTerm1 = 4*pbrt::Pi * dr*dr*dr;
   Float denTerm2 = alpha_prime*(d - zv) * (1.f + sigma_tr*dv) * exp(-sigma_tr*dv);
@@ -44,3 +60,6 @@ Float DipoleSolver::T(Float r)
 
   return (denTerm1 / numTerm1) - (denTerm2 / numTerm2);
 }
+
+
+// -------------------------- Multipole Solver -----------------------------------------------------------
