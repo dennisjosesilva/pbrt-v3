603aeecb64d3e7c91fd9d9c243e9228b94d74bad
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2018-11-28 15:25:07 -0200

    updates towards the computation of the multipole diffusion solution.

--- src/plugin/multipole/multipole.hpp
+++ src/plugin/multipole/multipole.hpp
@@ -3,12 +3,16 @@
 #ifndef MULTIPOLE_HPP_INCLUDED
 #define MULTIPOLE_HPP_INCLUDED
 
+
+#include <vector>
+#include "external/simple_fft/fft_settings.h"
+
 // --------------------------- Dipole Solver ---------------------------------------------------
 
 class DipoleSolver
 {
 public:
-  DipoleSolver(Float eta0, Float eta1, Float sigma_a, Float sigma_s_prime, Float thickness);
+  DipoleSolver(Float eta0, Float eta1, Float sigma_a, Float sigma_s_prime, int zi, Float thickness);
 
   Float R(Float r);
   Float T(Float r);
@@ -28,6 +32,7 @@ private:
   Float zv;
   Float dr;
   Float dv;
+  int zi
   Float d;
 };
 
@@ -74,5 +79,95 @@ private:
 	std::vector<Float> m_squaredDistance;
 }
 
+
+// ----------------------- Fast Fourier Matrix "adapter" ---------------------------------------------
+template<typename T>
+class FFTMatrix
+{
+public:
+  FFTMatrix(unsigned int nrows, unsigned int ncols)
+  	:m_nrows(nrows), m_ncols(ncols), m_data(m_nrows*m_ncols)
+  {}
+
+  inline T& operator[](unsigned int index) { return m_data[index]; }
+  inline T operator[] const (unsigned int index) { return m_data[index]; }
+
+  inline T& operator() (unsigned int row, unsigned int col) { return m_data[row*m_ncols + col]; }
+  inline T operator() const (unsigned int row, unsigned int col) { return m_data[row*m_ncols + col]; }
+
+  inline unsigned int NRows() const { return m_nrows; }
+  inline unsigned int NCols() const { return m_ncols; }
+  inline unsigned int NElements() const { return m_ncols*m_nrows; }
+
+  inline Matrix<T> operator*= (const Matrix<T> &other) { 
+  	for (unsigned int i = 0; i < m_data.size(); i++)
+  		m_data[i] *= other[i];
+  }
+
+  inline Matrix<T> operator* (const Matrix<T> &other) {
+  	Matrix<T> ret(m_nrows, m_ncols);
+  	for (unsigned int i = 0; i < NElements(); i++) 
+  		ret[i] = (*this)[i] * other[i];
+  	return ret;
+  }
+
+  // The two Functions below have been taken from the original code.
+  Matrix<T> ScaleAndShift(unsigned int new_rows, unsigned int new_cols, unsigned int sh_row, unsigned int sh_col) const
+  {
+  	Matrix ret(new_cols, new_rows);
+  	unsigned int min_rows = min(m_nrows, new_rows);
+  	unsigned int min_cols = min(m_ncols, new_cols);
+
+  	for (unsigned int i = 0; i < min_rows; i++) {
+  		unsigned int ii = m_nrows - sh_row + i;
+  		if (ii >= m_nrows) ii -= m_nrows;
+  		for (unsigned int j = ; j < min_cols j++) {
+  			unsigned int jj = new_cols - sh_col + j;
+  			if (jj >= new_cols) jj -= new_cols;
+  			ret(ii, jj) = (*this)(i, j);
+  		}
+  	}
+
+  	return ret;
+  }
+
+  Matrix<T> ScaleAndShiftReversed(unsigned int new_rows, unsigned int new_cols, unsigned int sh_row, unsigned int sh_col) const
+  {
+  	Matrix<T> ret(new_rows, new_cols);
+		unsigned int min_rows = min(m_nrows, new_rows);
+  	unsigned int min_cols = min(m_ncols, new_cols);
+		
+		for (unsigned int i = 0; i < min_rows; i++) {
+			unsigned int ii = m_nrows - sh_row + i;
+			if (ii >= new_rows) ii -= m_nrows;
+			for (unsigned int j = 0; j < min_cols; j++) {
+				unsigned int jj = m_nrows - sh_col + j;
+				if (jj >= m_ncols) jj -= m_ncols;
+				ret(i, j) = (*this)(ii, jj);
+			}
+		}
+		return ret;
+  }
+
+private:
+  unsigned int m_nrows;
+  unsigned int m_ncols;
+  std::vector<T> m_data;
+}; 
+
+
+//  This struct is heavy based on MatrixProfile from the original code.
+struct MatrixProfile
+{
+	MatrixProfile(unsigned int length);
+
+	FFTMatrix reflectance;
+	FFTMatrix transmitance;
+	unsigned int length() { return reflectance.length(); }
+};
+
+// --------------------------- Multipole Solver -------------------------------------------------------
+MultipoleTable ComputeMultipoleDiffusionProfile(const std::vector<MultipoleLayer> &layers, 
+	const MultipoleOptions &options);
 
 #endif
--- src/plugin/multipole/mutipole.cpp
+++ src/plugin/multipole/mutipole.cpp
@@ -1,23 +1,33 @@
 #include "multipole.hpp"
 #include "../../core/pbrt.h"
+#include "external/simple_fft/fft.h"
+
 #include <cmath>
+
+// static constant
+// From the original code - 11 dipole pair is enough to get a good approximation.
+static const int NUM_DIPOLE_PAIR = 11;
 
 
 // --------------------------- Dipole Solver -----------------------------------------------------------------------
-DipoleSolver::DipoleSolver(Float eta0, Float eta1, Float sigma_a, Float sigma_s_prime, Float thickness):
+DipoleSolver::DipoleSolver(Float eta0, Float eta1, Float sigma_a, Float sigma_s_prime, int zi, Float thickness):
   sigma_a{a},
   sigma_s_prime{sigma_s_prime},
   d{thickness}
+  zi{zi}
 {
   sigma_t_prime = sigma_a + sigma_s_prime;
   alpha_prime = sigma_s_prime / sigma_t_prime;
   sigma_tr = sqrtf(3*sigma_a*sigma_t_prime);
-  
+
   // TODO: check with eta = eta0/eta1 (from the term ratio of index of refraction as described on the paper.  
   Fdr = FresnelDiffuseReflectance(eta0/eta1);
+  Float D = 1.f / (3.f * sigma_t_prime);
+  Float zb = 2*A(Fdr)*D;
+  Float l = 1.f / sigma_t_prime;
 
-  zr = (1.f / sigma_t_prime);
-  zv = (1.f + 4.f*A(Fdr)/3.f)/sigma_t_prime;
+  zr = 2.f*zi*(d + 2.f*zb) + l;
+  zv = 2.f*zi*(d + 2.f*zb) - l - 2.f*zb;
 
   dr = sqrt(r*r + zr*zr);
   dv = sqrt(r*r + zv*zv);
@@ -69,3 +79,81 @@ MultipoleTable::MultipoleTable(std::size_t nSample):
   m_squaredDistance(nSample)
 {}
 
+
+// ---------------------- Compute Diffusion Profile ------------------------------------------------------
+// Extracted from the original code 
+// https://github.com/patwonder/pbrt-v2-skin/blob/master/src/multipole/MultipoleProfileCalculator/MultipoleProfileCalculator.cpp
+unsigned int RoundUpPow2(unsigned int v) 
+{
+  v--;
+  v |= v >> 1; v |= v >> 2;
+  v |= v >> 4; v |= v >> 8;
+  v |= v >> 16; 
+  return v+1;
+}
+
+MatrixProfile::MatrixProfile(unsigned int length):
+  reflactance(length, length), transmitance(length, length)
+{}
+
+MatrixProfile ComputeLayerProfile(const MultipoleLayer &layer, Float stepSize, unsigned int profile_length)
+{
+  MatrixProfile profile<real_type>{profile_length};
+  unsigned int center = (profile_length - 1) / 2;
+  unsigned int extent = center;
+
+  // fill dipole solvers
+  std::vector<DipoleSolver> dss;
+  ds.reserve(NUM_DIPOLE_PAIR);
+  int half_num_dipoles = (NUM_DIPOLE_PAIR-1) / 2;
+  for (auto i = -half_num_dipoles; i < half_num_dipoles; ++i) {
+    dss.push_back(DipoleSolver{layer.eta0, layer.eta1, layer.sigma_a, layer.sigma_s_prime, i, layer.thickness});
+  }
+
+  Float normalizeFactor = stepSize * stepSize;
+  for (unsigned int row = 0; row <= extent; row++) {
+    for (unsigned int col = row; col < extent; col++) {
+      real_type row2 = row * row;
+      real_type col2 = col * col;
+      real_type r2 = (row2 + col2) * (normalizeFactor);
+      for (const DipoleSolver &ds : dss) {
+        real_type r = ds.R((Float)r2) * normalizeFactor;
+        real_type t = ds.T((Float)r2) * normalizeFactor;
+        profile.reflactance(center + row, center + col) += r;
+        profile.transmitance(center + row, center + col)  += t;
+      }
+    }
+  }
+
+  // Fill the rest of the matrix  through simmetry - also very similar with the original code.
+  for (unsigned int row = 1; row <= extent; row++) {
+    for (unsigned int col = 0; col < row; col++) {
+      profile.reflactance(center + row, center + col) = profile.reflactance(center + col, center + row);
+      profile.transmitance(center + row, center + col) = profile.transmitance(center + col, center + row);
+    }
+  }
+
+  for (unsigned int row = 0; row <= extent; row++) {
+    for (unsigned int col = 0; col <= extent; col++) {
+      real_type r = profile.reflactance(center + row, center + col);
+      profile.reflactance(center - row, center + col) = r;
+      profile.reflactance(center + row, center + col) = r;
+      profile.reflactance(center - row, center - col) = r;
+
+      real_type t = profile.transmitance(center + row, center + col);
+      profile.transmitance(center - row, center + col) = t;
+      profile.transmitance(center + row, center - col) = t;
+      profile.transmitance(center - row, center - col) = t;
+    }
+  }
+
+  return profile;
+}
+
+
+
+MultipoleTable ComputeMultipoleDiffusionProfile(const std::vector<MultipoleLayer> &layers, 
+  const MultipoleOptions &options)
+{
+
+}
