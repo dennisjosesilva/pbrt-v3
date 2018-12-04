51c1785de6c49c08ca71d945a5f3bd769a6b10dc
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2018-12-04 16:17:34 -0200

    removing build folder from being tracked.

--- build/src/ext/openexr/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dennis/Documents/phd/courses/topics-in-computer-graphics/project/code/repo/pbrt-v3/src/ext/openexr
-# Build directory: /home/dennis/Documents/phd/courses/topics-in-computer-graphics/project/code/repo/pbrt-v3/build/src/ext/openexr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("IlmBase")
-subdirs("OpenEXR")
--- build/src/ext/ptex/src/utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dennis/Documents/phd/courses/topics-in-computer-graphics/project/code/repo/pbrt-v3/src/ext/ptex/src/utils
-# Build directory: /home/dennis/Documents/phd/courses/topics-in-computer-graphics/project/code/repo/pbrt-v3/build/src/ext/ptex/src/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
